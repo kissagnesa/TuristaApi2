e704bd5630335653d633b8a1239cbf6839e70a97
kissagnesa <sindzse88@gmail.com>
2025-04-08 18:01:54 +0200

    turaController is done

---
+++ Controllers/TuraController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TuristaApi2.Models;
+
+namespace TuristaApi2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TuraController : ControllerBase
+    {
+        [HttpPut("Modosit")]
+
+        public async Task<IActionResult> PutModosit(Tura tura)
+        {
+            try
+            {
+                using (var context = new TuristadbContext())
+                {
+                    context.Turas.Update(tura);
+                    await context.SaveChangesAsync();
+                    return Ok("Sikeres mentés.");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    return StatusCode(404, "Hiányzó túra!");
+                }
+                else
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}

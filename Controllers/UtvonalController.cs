d71152c1efe86d3efdf8bdcbf27997fd24b05efb
kissagnesa <sindzse88@gmail.com>
2025-04-08 16:43:24 +0200

    UtvonalController is done

--- Controllers/UtvonalController.cs
+++ Controllers/UtvonalController.cs
@@ -1,11 +1,55 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TuristaApi2.Models;
 
 namespace TuristaApi2.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UtvonalController : ControllerBase
     {
+        [HttpGet("All")]
+        public IActionResult GetUtvonal()
+        {
+            try
+            {
+                using (var context = new TuristadbContext())
+                {
+                    var utvonal = context.Utvonals.ToList();
+                    if (utvonal == null || utvonal.Count == 0)
+                    {
+                        return NotFound("Nincs elérhető útvonal.");
+                    }
+                    return Ok(utvonal);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+            }
+        }
+
+        [HttpGet("ById")]
+
+        public IActionResult GetUtvonalById(int id)
+        {
+            try
+            {
+                using (var context = new TuristadbContext())
+                {
+                    var utvonal = context.Utvonals.Include(f=>f.Allomasok).Include(f=>f.Tav).Include(f=>f.Nehezseg).FirstOrDefault(f => f.Id == id);
+                    if (utvonal == null)
+                    {
+                        return NotFound("Valószínűleg nincs ilyen túra.");
+                    }
+                    return Ok(utvonal);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status419AuthenticationTimeout, ex.Message);
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace TuristaApi2
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +16,8 @@ namespace TuristaApi2
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
